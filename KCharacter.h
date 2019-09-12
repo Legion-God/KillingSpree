77ec38d603e8f82e50d4d0c058e3580380b9abe8
Legion-God <47858236+Legion-God@users.noreply.github.com>
2019-09-12 11:50:43 +0530

    Add files via upload
    Header file for character movements

---
+++ KCharacter.h
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "KWeapon.h"
+#include "KCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class ROGUE_API AKCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AKCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	void BeginCrouch();
+
+	void EndCrouch();
+
+	bool bWantsToZoom;
+
+	UPROPERTY(EditDefaultsOnly, Category="Player")
+	float ZoomedFOV;
+
+	float DefaultFOV;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UCameraComponent* CameraComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USpringArmComponent* SpringArmComp;
+
+
+	UPROPERTY(EditAnywhere, Category = "Player", meta = (ClampMin = 0.1, ClampMax = 100))
+	float ZoomInSpeed;
+
+	void BeginZoom();
+
+	void EndZoom();
+
+	UPROPERTY(EditAnywhere, Category = "Player")
+	float CamLag;
+
+	AKWeapon* CurrentWeapon;
+
+	UPROPERTY(EditDefaultsOnly, Category="Player")
+	TSubclassOf<AKWeapon> StarterWeaponClass;
+
+	UPROPERTY(VisibleDefaultsOnly, Category= "Player")
+	FName WeaponAttachSocketName;
+
+	void Fire();
+
+	UPROPERTY(EditAnywhere, Category = "Player")
+	float SprintMultiplier;
+
+	void Sprint();
+
+	void EndSprint();
+
+	float DefaultMaxWalkSpeed=900.0f;
+	
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual FVector GetPawnViewLocation() const override; //gets pawn eye location
+															//override redefines base implementation
+};
