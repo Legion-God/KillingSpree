cfd3b039525c8b1054b07db6be19f5050816af99
Legion-God <47858236+Legion-God@users.noreply.github.com>
2019-09-12 11:51:10 +0530

    Add files via upload

---
+++ KWeapon.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "KWeapon.generated.h"
+
+class USkeletalMeshComponent;
+class UDamageType;
+class UParticleSystem;
+
+UCLASS()
+class ROGUE_API AKWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AKWeapon();
+
+protected:
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Components")
+	USkeletalMeshComponent* MeshComp;
+
+	
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gun_Mechanics")
+		int Range = 10000; //defines the range of the weapon., vary this for different class of weapons 
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon")
+		float baseDamage = 20.0f;
+
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon")
+	TSubclassOf <UDamageType> DamageType;
+
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	FName MuzzleSocketName;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	FName TracerTargetName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	UParticleSystem* MuzzleEffect;
+
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	UParticleSystem* ImpactEffect;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	UParticleSystem* TracerEffect;
+
+
+	void PlayFireEffects(FVector TracerEndPoint);
+
+
+	UPROPERTY(EditAnywhere, Category = "Weapon")
+	int RateOfFire;
+
+	
+public:	
+		UFUNCTION(BlueprintCallable, Category="Weapon")
+		virtual void Fire();
+
+};
